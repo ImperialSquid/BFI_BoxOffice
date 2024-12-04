d0e2e609c9421abbd96cc224c567f944f17f8e4a
Ben Jaeger <benjaeger99@gmail.com>
2024-12-04 23:39:31 +0000

    fix: only run script to get URLs if variable doesn't exist
    Since we cache `files` at the end, depending on whether load_cache and/or load_data is TRUE, we might try to run this scrpit again, this is a small optimisation to check if it's worth it first

--- bfi_boxoffice/data/01_get_spreadsheet_links.R
+++ bfi_boxoffice/data/01_get_spreadsheet_links.R
@@ -1,48 +1,52 @@
 library("tidyverse")  # require rvest for scraping, magnittr for pipes, dplyr for other verbs
 library("polite")  # it's good to be civil :)
 
-
-host = "https://www.bfi.org.uk"
-session = bow(host)
-
-first_page = "/industry-data-insights/weekend-box-office-figures"
-
-# get all links from BFI spreadsheets landing page
-links = bow(host) %>% 
-  nod(first_page) %>% 
-  scrape() %>% 
-  rvest::html_elements("a") %>% 
-  html_attrs_dfr() %>% 
-  data.frame()
-
-# some weekends have direct links here
-files = links %>% 
-  filter(grepl("download", href)) %>% 
-  select(href)
-
-# older weekends are split into pages for previous years
-pages = links %>% 
-  filter(grepl("industry-data-insights/weekend-box-office-figures", href)) %>% 
-  # select(href) %>% 
-  pull(href)
-
-# print(pages)
-
-# iter through previous years to get those spreadsheet links
-for (p in pages) {
-  files = session %>% 
-    nod(p) %>% 
+if (exists("files")) {
+  print("Variable `files` already exists, skipping data script...")
+} else {
+  host = "https://www.bfi.org.uk"
+  session = bow(host)
+  
+  first_page = "/industry-data-insights/weekend-box-office-figures"
+  
+  # get all links from BFI spreadsheets landing page
+  links = bow(host) %>% 
+    nod(first_page) %>% 
     scrape() %>% 
     rvest::html_elements("a") %>% 
     html_attrs_dfr() %>% 
-    data.frame() %>% 
+    data.frame()
+  
+  # some weekends have direct links here
+  files = links %>% 
     filter(grepl("download", href)) %>% 
-    select(href) %>% 
-    bind_rows(files)
+    select(href)
+  
+  # older weekends are split into pages for previous years
+  pages = links %>% 
+    filter(grepl("industry-data-insights/weekend-box-office-figures", href)) %>% 
+    # select(href) %>% 
+    pull(href)
+  
+  # print(pages)
+  
+  # iter through previous years to get those spreadsheet links
+  for (p in pages) {
+    files = session %>% 
+      nod(p) %>% 
+      scrape() %>% 
+      rvest::html_elements("a") %>% 
+      html_attrs_dfr() %>% 
+      data.frame() %>% 
+      filter(grepl("download", href)) %>% 
+      select(href) %>% 
+      bind_rows(files)
+  }
+  
+  # cache file URLs
+  cache("files")
+  
+  # clean up
+  rm(links, session, first_page, host, p, pages)
+
 }
-
-# cache file URLs
-cache("files")
-
-# clean up
-rm(links, session, first_page, host, p, pages)
