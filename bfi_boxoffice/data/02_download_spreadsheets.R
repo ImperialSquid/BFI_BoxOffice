976e8df101106c6ba419c8f29539d5bdcc925980
Ben Jaeger <benjaeger99@gmail.com>
2024-12-07 20:29:27 +0000

    fix: better spreadhseet downloading
    Download spreadsheets to files with the same IDs for future proofing, check if file exists before downloading to avoid unnecessary networking

--- bfi_boxoffice/data/02_download_spreadsheets.R
+++ bfi_boxoffice/data/02_download_spreadsheets.R
@@ -5,7 +5,11 @@ host = files$href[1]
 session = bow(host)
 
 # file URLs from previous step
-files_list = files %>% pull(href)
+files_list = files %>% 
+  pull(href)
+files_ids = files %>% 
+  mutate(id_ = gsub(".*?(\\d+).*", "\\1", href, perl = TRUE)) %>%
+  pull(id_)
 
 # download to /data dir (where this script is)
 download_dir = paste0(dirname(rstudioapi::getActiveDocumentContext()$path),
@@ -15,14 +19,20 @@ download_dir = paste0(dirname(rstudioapi::getActiveDocumentContext()$path),
 for (index in 1:length(files_list)) {
   print(paste0("Downloading spreadsheet ", index, " of ", length(files_list)))
   
-  session %>% 
-    nod(files_list[index]) %>% 
-    rip(path = paste0(download_dir,
-                      "/spreadsheets"),
-        destfile = paste0("spreadsheet_", 
-                          str_pad(index, 3, pad = "0")))  
-  # don't specify file type, some are xls, some are xlsx _sigh_
+  if (file.exists(paste0(download_dir,
+                         "/spreadsheets/",
+                         files_ids[index]))) { 
+    print("Spreadsheet already exists")
+  } else {
+    session %>% 
+      nod(files_list[index]) %>% 
+      rip(path = paste0(download_dir,
+                        "/spreadsheets"),
+          destfile = files_ids[index])
+    print("Success!")
+    # don't specify file type, some are xls, some are xlsx _sigh_
+  }
 }
 
 # clean up
-rm(host, session, files_list, download_dir, index)
+rm(host, session, files_list, files_ids, download_dir, index)
